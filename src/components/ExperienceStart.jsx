0a393f70484d2b5cc9af73e57e9a3b206e6fd95a
Hafiz <hafizurrahman.6dof@gmail.com>
2025-12-23 16:12:17 +0530

    feat: Add ExperienceStart component to provide an interactive entry screen with animated tagline and button.

--- src/components/ExperienceStart.jsx
+++ src/components/ExperienceStart.jsx
@@ -1,10 +1,18 @@
 import './ExperienceStart.scss';
 
 export default function ExperienceStart({ onStart }) {
+    const tagline = "BORN TO BUILD. FORGED IN CODE.";
+
     return (
         <div className="experience-start">
             <div className="start-content">
-                <h1>J Md Hafizur Rahman</h1>
+                <h1 className="animated-tagline">
+                    {tagline.split("").map((char, index) => (
+                        <span key={index} className="char">
+                            {char === " " ? "\u00A0" : char}
+                        </span>
+                    ))}
+                </h1>
                 <p>Immersive Audio Experience Ready</p>
                 <button onClick={onStart}>
                     Enter Cockpit
